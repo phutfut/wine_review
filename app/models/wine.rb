44c02057e21811251e6a95f1a4124c09b14aca8d
Wellson <wellson.fung@gmail.com>
2015-05-02 06:21:11 -0700

    added select on the form

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -7,4 +7,7 @@ class Wine < ActiveRecord::Base
   validates :year,
     numericality: { only_integer: true, greater_than_or_equal_to: 0 },
     unless: "year.blank?"
+
+  validates :varietal,
+    inclusion: VARIETALS
 end
